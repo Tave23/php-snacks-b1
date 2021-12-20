acf43189cb3310fb342c75e25fb7e51aeaa625d4
Davide Tavella <tavella01@gmail.com>
2021-12-20 13:36:23 +0100

    array dei post

---
+++ snack-3/index.php
@@ -0,0 +1,55 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Massimo Moratti',
+            'text' => 'suscipit iste nobis corrupti voluptates dolorem'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Gerry Scotti',
+            'text' => 'suscipit iste nobis corrupti voluptates dolorem'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Papa Francesco',
+            'text' => 'Voluptas hic aspernatur optio et, molestiae voluptate laboriosam maiores! Quasi ratione consequuntur atque aliquam voluptates magnam beatae qui debitis dolorum commodi.'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michelle Hunkizer',
+            'text' => 'sit amet, amet necessitatibus?'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Alberto Tomba',
+            'text' => 'sit amet, consectetur adipisicing elit. Dolore corporis illum delectus deleniti officiis eum expedita ipsa iusto eligendi ducimus dolor molestias commodi repudiandae optio, amet necessitatibus?'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Sergio Mattarella',
+            'text' => 'Voluptas hic aspernatur optio et, molestiae voluptate laboriosam maiores! Quasi ratione consequuntur atque aliquam voluptates magnam beatae qui debitis dolorum commodi.'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Snack 3</title>
+</head>
+<body>
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore corporis illum delectus deleniti officiis eum expedita ipsa iusto eligendi ducimus dolor molestias commodi repudiandae optio, amet necessitatibus? A est quae sit numquam voluptas repellendus, illo ab esse earum quos et suscipit iste nobis corrupti voluptates dolorem veritatis consequuntur obcaecati accusantium voluptatum illum voluptate nisi aperiam. Id ex, at odit consectetur quibusdam consequatur dicta quod necessitatibus veritatis ipsam minus dolores quis, laudantium fugit est quasi vero molestiae corrupti! Veritatis, sunt! Voluptas hic aspernatur optio et, molestiae voluptate laboriosam maiores! Quasi ratione consequuntur atque aliquam voluptates magnam beatae qui debitis dolorum commodi.
+</body>
+</html>
