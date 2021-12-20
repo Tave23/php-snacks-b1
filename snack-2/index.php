5a1a09b7f62981e4b190d0794529210d1c56fa1b
Davide Tavella <tavella01@gmail.com>
2021-12-20 13:36:07 +0100

    first commit

--- snack-2/index.php
+++ snack-2/index.php
@@ -27,7 +27,7 @@ if (strlen($name) > 1 && strpos($mail, "@") && strpos($mail, ".") && is_numeric(
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Snack PHP</title>
+   <title>Snack 2</title>
 </head>
 <body>
    
